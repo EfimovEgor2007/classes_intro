e5d89de08fa54627da026c7d681dfe9f0fc0e0fa
iliakakurin <49334425+iliakakurin@users.noreply.github.com>
2019-06-01 08:05:50 +0500

    Create complex functions using simple ones

--- classes_intro.py
+++ classes_intro.py
@@ -1,14 +1,38 @@
-# add your code from task 1 between the lines
-#---------------------------------------------------------------------------
+#creating a class
+class Things:
+    pass
+#creating a derived class (from base class)
+class Inanimate(Things):
+    pass
 
-#---------------------------------------------------------------------------
+class Animate(Things):
+    def exist(self):
+      print('Cogito ergo sum')
 
-# TODO 
-# 1 create a Giraffes object 'reginald'
-# 2 make reginald move, breathe and eat leaves from trees
-# 3 create another object 'infusorium' - which class does it refer to?
-# 4 make your infusorium exist
-# 5 check if infusorium can move, eat or breathe
+class Sidewalks(Inanimate):
+    pass
 
-# 1
-reginald = #...
+class Animals(Animate):
+    def breathe(self):
+        print('breathes')
+    def move(self):
+        print('moves')
+    def eat(self):
+        print('eats some food')
+
+class Mammals(Animals):
+    def feed_baby_with_milk(self):
+        print('feeds the baby with milk')
+
+class Giraffes(Mammals):
+    # usually in life complex functions and actions consist of simpler, iherited functions
+    # example - how to use function eat() in eat_leaves_from_trees()
+    def eat_leaves_from_trees(self):
+        self.eat()
+        print('right from the trees')
+    def eat_rainbow_from_the_sky(self):
+        # TODO - eat() it, for Skittles sake!
+    def find_food(self):
+      # TODO - move() your ass, find some food, eat() it!
+    def dance_jig(self):
+      # TODO - move(), move(), move(), move() your body!
